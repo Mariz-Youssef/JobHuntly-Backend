ebace1a9255d6b6d063aa437d543d694b9e4d0a1
MoazRyhan <moazryhan@gmail.com>
2026-01-03 01:30:53 +0200

    add the missing parts in the apis

--- src/Modules/settings/services/SettingsApplicant.service.js
+++ src/Modules/settings/services/SettingsApplicant.service.js
@@ -221,3 +221,49 @@ export const deleteAccount = async (userId, role, password) => {
   // ❌ delete user
   await UserModel.findByIdAndDelete(userId)
 }
+
+
+
+/*
+========================================== Applicant =============================
+
+
+💼 Experiences
+POST   http://localhost:3000/users/me/experiences
+DELETE http://localhost:3000/experiences/:id
+
+🎓 Educations
+POST   http://localhost:3000/users/me/educations
+DELETE http://localhost:3000/educations/:id
+
+🧠 Skills
+DELETE http://localhost:3000/users/me/skills/:id
+
+
+====================================================
+
+POST   http://localhost:3000/users/me/avatar
+
+📝 background image
+PUT    http://localhost:3000/users/me/BG-image
+
+
+
+
+👤 My Profile (الصفحة العامة)
+PUT    http://localhost:3000/users/me/profile 🫸
+GET    http://localhost:3000/users/me/profile 🫸
+
+
+⚙️ Settings → Login Details
+تغيير الإيميل
+PUT    http://localhost:3000/settings/change-email 🫸
+
+تغيير الباسورد
+PUT    http://localhost:3000/settings/reset-password 🫸
+
+❌ Delete account
+DELETE http://localhost:3000/settings/delete-account 🫸
+
+
+*/
